f362bd1a27134de4acdf3b93adfe741fef2586a8
Gordon Pedersen <gordon@aerion.com.au>
2019-04-02 13:45:10 +1100

    Add tray messages functionality to basecamp

--- appstore/basecamp3/basecamp.js
+++ appstore/basecamp3/basecamp.js
@@ -6,8 +6,27 @@
             prev = next;
         }
     }
+    function getMessages() {
+        BC.fetch(BC.accountPath("/my/readings"), { as: "json" }).then((e) => {
+            let messages = [];
+            for (const i in e.unreads) {
+                if (e.unreads.hasOwnProperty(i)) {
+                    const unread = e.unreads[i];
+                    messages.push({
+                        id: unread.id,
+                        title: unread.title,
+                        subtitle: `${unread.creator.name} - ${unread.bucket_name}`,
+                        date: Date.parse(unread.unread_at)
+                    });
+                }
+            }
+            window.wavebox.tray.setMessages(messages);
+        });
+    }
     document.addEventListener("bc:unreads-change", function (e) {
         setCount(e.unreads.length);
+        getMessages();
     });
     setCount(BC.unreads.getTotal());
+    getMessages();
 })()
